be16f7c905257b11442c1d6b22b26b39649922df
a4200 <a4200@users.noreply.github.com>
2022-06-02 17:51:43 +0200

    region change

--- create.sh
+++ create.sh
@@ -2,5 +2,5 @@ aws cloudformation create-stack \
 --stack-name $1 \
 --template-body file://$2 \
 --parameters file://$3 \
---region=eu-west-2 \
+--region=us0east-1 \
 --capabilities CAPABILITY_NAMED_IAM
